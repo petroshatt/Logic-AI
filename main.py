5d7c23fb6fe3e79a964e108c2a7404a9da75d241
Petros Chatzitoulousis <petroshatt@gmail.com>
2022-01-06 19:46:01 +0200

    started GSAT

--- main.py
+++ main.py
@@ -1,30 +1,25 @@
 #Logic AI - Petros Chatzitoulousis
 import random
 
-KB = []
-
 
 def constructKB():
 
-    var_num = input("Enter the number of Logical Variables (1-26): ")
-    var_num = int(var_num)
+    kb = []
+
+    var_num = int(input("Enter the number of Logical Variables (1-26): "))
     while (var_num < 1) or (var_num > 26):
-        var_num = input("Try again! Enter a number between 1 and 26: ")
-        var_num = int(var_num)
+        var_num = int(input("Try again! Enter a number between 1 and 26: "))
 
-    lit_num = input("Enter the number of Literals: ")
-    lit_num = int(lit_num)
+    lit_num = int(input("Enter the number of Literals: "))
     while (lit_num < 1) or (lit_num > var_num):
-        lit_num = input("Try again! Enter a number greater that 0: ")
-        lit_num = int(lit_num)
+        lit_num = int(input("Try again! Enter a number greater that 0: "))
 
-    sent_num = input("Enter the number of Sentences: ")
-    sent_num = int(sent_num)
+    sent_num = int(input("Enter the number of Sentences: "))
     while sent_num < 1:
-        sent_num = input("Try again! Enter a number greater that 0: ")
-        sent_num = int(sent_num)
+        sent_num = int(input("Try again! Enter a number greater that 0: "))
 
     sent_counter = 0
+    var_used = []
     while sent_counter < sent_num:
 
         var_list = []
@@ -38,6 +33,9 @@ def constructKB():
 
             lit = random.choice(var_list)
             var_list.remove(lit)
+            if lit not in var_used:
+                var_used.append(lit)
+                var_used.sort()
 
             negative_possibility = random.choice([0, 1])
             if negative_possibility == 1:
@@ -45,19 +43,37 @@ def constructKB():
 
             sent.append(lit)
 
-        if sent not in KB:
-            KB.append(sent)
+        if sent not in kb:
+            kb.append(sent)
             sent_counter += 1
-
-    for s in KB:
-        print(*s)
 
     f = open("kb.txt", "w")
     f.write("Number of Logical Variables: " + str(var_num) + " | Number of Sentences: " + str(sent_num) + " | Max Literals in each Sentence: " + str(lit_num) + "\n")
-    for s in KB:
+    for s in kb:
         s = str(s).replace("[", "").replace("'", "").replace("]", "").replace(",", "").replace(" ", " ∨ ")
         f.write(s + "\n")
     f.close()
+
+    return kb, var_used
+
+
+def GSAT(KB, var_used, maxTries, maxFlips):
+
+    var_values = {}
+
+    for i in range(maxTries):
+
+        for lit in var_used:
+            var_values[lit] = random.choice([True,False])
+
+        current_cost = sum(1 for v in var_values.values() if v == False) #counts the Falses
+
+        # for j in range(maxFlips):
+        #
+        #     if satisfies(var_values, KB):
+        #         return var_values
+        #
+        #     else
 
 
 
@@ -66,4 +82,10 @@ def constructKB():
 
 
 if __name__ == '__main__':
-    constructKB()
+
+    KB, var_used = constructKB()
+
+    GSAT(KB, var_used, 100, 100)
+
+    # for s in KB:
+    #     print(*s)
